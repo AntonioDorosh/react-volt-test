ce2a245f87efbb358ff479773112d2fe98f4516b
AntonioDorosh <antondoroshenko92@gmail.com>
2024-03-07 21:48:44 +0200

    disable btn

--- src/components/TodoInput/index.tsx
+++ src/components/TodoInput/index.tsx
@@ -1,52 +1,57 @@
-import React, { ChangeEvent, FC, FormEvent, useEffect, useState } from "react"
-import styles from "./TodoInput.module.scss"
-import {useDebounce} from "../../hooks/useDebouce";
-import {useAppDispatch} from "../../features/redux/hooks/useStore";
-import {Todo} from "../../features/redux/reducers/todo/type";
-import {addTask} from "../../features/redux/reducers/todo/slice";
+import React, { ChangeEvent, FC, FormEvent, useEffect, useState } from "react";
+import styles from "./TodoInput.module.scss";
+import { useDebounce } from "../../hooks/useDebouce";
+import { useAppDispatch } from "../../features/redux/hooks/useStore";
+import { addTask } from "../../features/redux/reducers/todo/slice";
 
 const TodoInput: FC = () => {
-    const [inputTaskValue, setInputTaskValue] = useState("")
-    const debounceInputValue = useDebounce(inputTaskValue, 500)
-    const dispatch = useAppDispatch()
+  const [inputTaskValue, setInputTaskValue] = useState("");
+  const debounceInputValue = useDebounce(inputTaskValue, 500);
+  const dispatch = useAppDispatch();
 
-    useEffect(() => {
-        setInputTaskValue(debounceInputValue)
-    }, [debounceInputValue])
+  useEffect(() => {
+    setInputTaskValue(debounceInputValue);
+  }, [debounceInputValue]);
 
-    const addTodo = (event: FormEvent<HTMLFormElement>): void => {
-        event.preventDefault()
+  const addTodo = (event: FormEvent<HTMLFormElement>): void => {
+    event.preventDefault();
 
-        if (inputTaskValue.length < 3 || inputTaskValue.trim() === "") {
-            alert("You need at least 3 letter to add some task")
-            return
-        }
-
-        dispatch(addTask({
-                id: crypto.randomUUID(),
-                title: inputTaskValue,
-                isCompleted: false,
-            }))
-        setInputTaskValue("")
+    if (inputTaskValue.length < 3 || !inputTaskValue.trim()) {
+      alert("You need at least 3 letter to add some task");
+      return;
     }
 
-    const inputTaskValueHandler = (event: ChangeEvent<HTMLInputElement>): void =>
-        setInputTaskValue(event.target.value)
+    dispatch(
+      addTask({
+        id: crypto.randomUUID(),
+        title: inputTaskValue,
+        isCompleted: false,
+      }),
+    );
+    setInputTaskValue("");
+  };
 
-    return (
-        <form className={styles.form} onSubmit={addTodo}>
-            <input
-                value={inputTaskValue}
-                onChange={inputTaskValueHandler}
-                className={styles.form__input}
-                type="text"
-                placeholder={"Enter your task here..."}
-            />
-            <button className={styles.form__button} type="submit">
-                Add
-            </button>
-        </form>
-    )
-}
+  const inputTaskValueHandler = (event: ChangeEvent<HTMLInputElement>): void =>
+    setInputTaskValue(event.target.value);
 
-export default TodoInput
+  return (
+    <form className={styles.form} onSubmit={addTodo}>
+      <input
+        value={inputTaskValue}
+        onChange={inputTaskValueHandler}
+        className={styles.form__input}
+        type="text"
+        placeholder={"Enter your task here..."}
+      />
+      <button
+        className={styles.form__button}
+        type="submit"
+        disabled={!inputTaskValue || !inputTaskValue.trim()}
+      >
+        Add
+      </button>
+    </form>
+  );
+};
+
+export default TodoInput;
